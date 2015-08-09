472a82c20f70fc21147ff0c813462bdd848f6cf6
Chris Coe <chrcoe@ieee.org>
2015-08-09 19:34:22 -0400

    fixed bootstrap/jquery imports on async index page

--- appname/client/e2e-tests/scenarios.js
+++ appname/client/e2e-tests/scenarios.js
@@ -4,34 +4,30 @@
 
 describe('my app', function() {
 
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /cookieview when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/cookieview");
   });
 
-
-  describe('view1', function() {
+  describe('cookieview', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/cookieview');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
+    it('should render cookieview when user navigates to /cookieview', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
         toMatch(/partial for view 1/);
     });
 
   });
 
-
   describe('view2', function() {
 
     beforeEach(function() {
       browser.get('index.html#/view2');
     });
-
 
     it('should render view2 when user navigates to /view2', function() {
       expect(element.all(by.css('[ng-view] p')).first().getText()).
